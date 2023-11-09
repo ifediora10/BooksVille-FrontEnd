9a96c75b4a0650e1274ecff7b6ecd547d6d88ece
Danny4life <osiki.segun@gmail.com>
2023-11-09 16:11:13 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div> 
-      <h1 className="text-red-700">Welcome to the live project</h1>
+      <h1 className="text-green-700">Welcome to the SQD 17 live project!!!</h1>
       </div>
     </>
   )
