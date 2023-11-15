a2b86283264e1abf31bf72f1f8bd46111540934e
onyekanma2006 <onyekanma2006@gmail.com>
2023-11-15 23:21:06 +0100

    completion of the static login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
-
+import {LoginForm} from "./component/LoginForm.jsx";
+import {ForgotPassword} from "./component/ForgotPassword.jsx";
 
 function App() {
   
 
   return (
     <>
-      <div> 
-      <h1 className="text-green-700">Welcome to the SQD 17 live project!!!</h1>
+      <div>
+          <LoginForm/>
+          {/*<ForgotPassword/>*/}
       </div>
     </>
   )
