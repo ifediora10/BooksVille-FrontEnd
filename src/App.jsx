16959ab6c0a2dcbd4ede320d422f033ada54f6d8
Olivia <ikajiubachidiogo@gmail.com>
2023-11-19 21:59:50 +0100

    static page for job application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import {ReviewApplication} from "./components/ReviewApplication.jsx";
+import {ApplicationProfileUpdate} from "./components/ApplicationProfileUpdate.jsx";
+import {FeatureButton} from "./components/FeatureButton.jsx";
 
 
 function App() {
 
   return (
       <>
-
+          <ReviewApplication/>
       </>
   )
 }
