847350007bc3027b0682d0e41a0e74c64cbb707f
Oluyinka Odebode <75814065+coolgenius2@users.noreply.github.com>
2023-11-20 14:38:12 +0100

    created component for post new job - contact details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import {SignUpForm} from "./components/SignUpForm.jsx";
+import {Feature} from "./components/Feature.jsx";
+import {PostNewJobContactDetailsPage} from "./components/PostNewJobContactDetailsPage.jsx";
 
 
 function App() {
-  
 
   return (
     <>
-        <SignUpForm />
+        <PostNewJobContactDetailsPage/>
     </>
   )
 }
