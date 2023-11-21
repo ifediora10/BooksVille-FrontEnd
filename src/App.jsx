801e55d37bcb50f523a86938aac0cfe64bf2b29a
user <sunday.peter@decagon.dev>
2023-11-21 22:43:46 +0100

    implement jobseeker and employer profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import {LoginForm} from "./components/LoginForm.jsx";
+import {LoginForm} from "./components/profile/Authentication/LoginForm.jsx";
+import {JobSeekerProfile} from "./components/profile/jobseekerProfile/JobSeekerProfile.jsx";
+import {EmployerProfile} from "./components/profile/employerProfile/EmployerProfile.jsx";
+import {Panel} from "./components/profile/profileComponents/Panel.jsx";
+import {PersonalInfo} from "./components/profile/jobseekerProfile/personalInfo/PersonalInfo.jsx";
+import {JobPreference} from "./components/profile/jobseekerProfile/jobPreference/JobPreference.jsx";
 
 
 function App() {
-  
+
 
   return (
     <>
-        <LoginForm/>
+        {/*<LoginForm/>*/}
+        <JobSeekerProfile/>
+        {/*<EmployerProfile/>*/}
+        {/*<Panel/>*/}
+        {/*<PersonalInfo/>*/}
+        {/*<JobPreference/>*/}
     </>
+
   )
 }
 
 export default App
+
+
