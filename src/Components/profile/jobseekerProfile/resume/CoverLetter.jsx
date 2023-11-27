6743bdc796e3b3eb76098ff43424bddd902553e4
user <sunday.peter@decagon.dev>
2023-11-28 00:44:22 +0100

    coverletter and resume update frontend

--- src/Components/profile/jobseekerProfile/resume/CoverLetter.jsx
+++ src/Components/profile/jobseekerProfile/resume/CoverLetter.jsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import {useState} from "react";
 import {useDropzone} from "react-dropzone";
+import axios from "../../../../api/axios.jsx";
+import {SweetAlert} from "../../../utils/SweetAlert.jsx";
 
 export const CoverLetter = () => {
     const [uploadedFiles, setUploadedFiles] = useState([]);
@@ -13,8 +15,36 @@ export const CoverLetter = () => {
         },
     });
 
+    function uploadSingleFile(coverletter) {
+        const formData = new FormData();
+        formData.append('coverLetter', coverletter);
+
+        // Retrieve the JWT token from your authentication system (e.g., from localStorage)
+        const token = localStorage.getItem('token');
+
+        axios
+            .put("/job-seeker/update-profile/cover-letter", formData, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'multipart/form-data', // Specify content type for file upload
+                },
+            })
+            .then((result) => {
+                console.log(result.data)
+                SweetAlert('success', 'Update Successful', 'coverletter uploaded successfully');
+            })
+            .catch((error) => {
+                SweetAlert('error', 'Oops!', 'Something went wrong please try again', 2000);
+                console.error('Error uploading coverletter :', error);
+                // Handle error appropriately (e.g., show an error message to the user)
+            });
+    }
+
     return(
-        <form className="flex flex-col items-stretch w-[61%] ml-5 max-md:w-full max-md:ml-0">
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            uploadSingleFile(uploadedFiles[0]);
+        }} className="flex flex-col items-stretch w-[61%] ml-5 max-md:w-full max-md:ml-0">
             <div className="items-start flex grow flex-col max-md:max-w-full max-md:mt-10">
                 <div className="text-gray-300 text-sm leading-5 tracking-normal whitespace-nowrap flex justify-start items-stretch bg-white self-start w-fit ">
                     drop your cover letter in the box below
@@ -39,7 +69,7 @@ export const CoverLetter = () => {
                     type="submit"
                     name="submit"
                     value="Save Changes"
-                    className="text-white text-base font-semibold leading-6 tracking-normal whitespace-nowrap justify-center items-stretch bg-blue-500 w-fit max-w-full mt-8 px-4 py-2 rounded-xl self-end cursor-pointer"
+                    className="text-white text-base font-semibold leading-6 tracking-normal whitespace-nowrap justify-center items-stretch bg-blue-500 w-[145px] max-w-full mt-8 px-4 py-2 rounded-xl self-end cursor-pointer"
                 />
             </div>
         </form>
