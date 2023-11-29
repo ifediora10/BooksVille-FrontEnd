e11200d7c6f249b43f295b865c041397b47a61e1
onyekanma2006 <onyekanma2006@gmail.com>
2023-11-29 08:24:24 +0100

    create new job page job information and integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import {LoginForm} from "./components/LoginForm.jsx";
+import {JobInformation} from "./components/JobInformation.jsx"
+
 
 
 function App() {
@@ -6,7 +8,8 @@ function App() {
 
   return (
     <>
-        <LoginForm/>
+        {/*<LoginForm/>*/}
+        <JobInformation/>
     </>
   )
 }
